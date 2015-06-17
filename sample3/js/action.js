aa25b1eea6f6d635f3abc7a852f92d2f813ebef7
makotodesign <makoto@makotodesign.net>
2015-06-17 18:14:58 +0900

    スライドとりあえず完成

--- sample3/js/action.js
+++ sample3/js/action.js
@@ -1,20 +1,21 @@
 $(function(){
-//topslider
+//topslideshow
 $('#bg1').css({opacity:1});
 var no = 1;
 var max = $('#top article').length;
-/*
-$('#bg'+no).animate({opacity:1},2000,'linear').delay(3000).animate({opacity:0},2000,'linear');	
-*/
 function timer() {
-	$('#bg'+no).animate({opacity:1},2000,'linear').delay(4000).animate({opacity:0},2000,'linear');
+	$('#bg'+no).animate({opacity:0},3000,'linear');
+	$('#bg'+(1+no)).animate({opacity:1},3000,'linear');
 	no++;
 	if(no>max){
 		no=1;
+		$('#bg1').animate({opacity:1},3000,'linear');
 	}
 }
+setInterval(timer,9000);
 
-	setInterval(timer,8000);
+
+
 
 
 
