57fbf3b384e083ba90dd474e39c4b254a54b3906
makotodesign <makoto@makotodesign.net>
2015-06-17 20:47:40 +0900

    order1箱アクション完成

--- sample3/js/action.js
+++ sample3/js/action.js
@@ -13,15 +13,38 @@ function timer() {
 	}
 }
 setInterval(timer,9000);
+//orderpage_action
+/*
+$('.order').hover(function(){
+	$(this).css({backgroundColor: '#11B8C4',color:'#fff'})
+},function(){
+	$(this).css({backgroundColor: '#EFE8E8',color:'#000'})
+});
+*/
+$('#order1 h2').click(function(){
+	$('#order2').fadeOut();
+	$('#order3').fadeOut();
+	$(this).parent().animate({height:'95%',top:'-5%'},1000,'linear',function(){
+		$('#order1 h2').fadeOut();
+		$('#close1').fadeIn();
+	});
+});
+$('#close1').click(function(){
+	$('#order1').animate({height:'25%',top:'0%'},1000,'linear',function(){
+	$('#order2').fadeIn();
+	$('#order3').fadeIn();
+	$('#order1 h2').fadeIn();
+	$('#close1').fadeOut();		
+	})
 
-
-
-
-
+});
 
 
 
 });
+
+
+
 
 
 
