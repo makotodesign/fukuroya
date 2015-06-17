83af3453e320236dbe731aac620545519865ea73
makotodesign <makoto@makotodesign.net>
2015-06-18 01:33:06 +0900

    order1の中の項目完了

--- sample3/js/action.js
+++ sample3/js/action.js
@@ -15,30 +15,34 @@ function timer() {
 setInterval(timer,9000);
 //orderpage_action
 //order1
-$('#order1 h2').click(function(){
+$('#order1>h2').click(function(){
 	$('#order2').fadeOut();
 	$('#order3').fadeOut();
-	$('#order1 h2').animate({fontSize:'500%'},1000,'easeOutBounce').delay(1000);
+	$('#order1>h2').animate({fontSize:'500%'},1000,'easeOutBounce').delay(1000);
 	$('#order1').animate({height:'95%',top:'-5%'},1000,'easeOutExpo',function(){
-		$('#order1 h2').fadeOut();
 		$('#close1').fadeIn();
+		$('#order1>h2').fadeOut(1000,function(){
+			$('#order1_inner').fadeIn();
+		});
+
 	});
 });
 $('#close1').click(function(){
+	$('.order_inner').fadeOut(100);
 	$('#order1').animate({height:'25%',top:'0%'},1000,'easeOutExpo',function(){
 	$('#order2').fadeIn();
 	$('#order3').fadeIn();
-	$('#order1 h2').css({fontSize:'300%'}).fadeIn();
+	$('#order1>h2').css({fontSize:'300%'}).fadeIn();
 	$('#close1').fadeOut();		
 	})
 
 });//order2
-$('#order2 h2').click(function(){
+$('#order2>h2').click(function(){
 	$('#order3').fadeOut();
-	$('#order2 h2').animate({fontSize:'500%'},1000,'easeOutBounce').delay(1000);
+	$('#order2>h2').animate({fontSize:'500%'},1000,'easeOutBounce').delay(1000);
 	$('#order1').fadeOut(300,function(){
 		$('#order2').animate({top:'-5%',height:'95%'},1000,'easeOutExpo',function(){
-			$('#order2 h2').fadeOut();
+			$('#order2>h2').fadeOut();
 			$('#close2').fadeIn();
 		});
 
@@ -48,17 +52,17 @@ $('#close2').click(function(){
 	$('#order2').animate({height:'25%',top:'30%'},1000,'easeOutExpo',function(){
 	$('#order1').fadeIn();
 	$('#order3').fadeIn();
-	$('#order2 h2').css({fontSize:'300%'}).fadeIn();
+	$('#order2>h2').css({fontSize:'300%'}).fadeIn();
 	$('#close2').fadeOut();		
 	})
 });
 //order3
 $('#order3 h2').click(function(){
 	$('#order2').fadeOut();
-	$('#order3 h2').animate({fontSize:'500%'},1000,'easeOutBounce').delay(1000);
+	$('#order3>h2').animate({fontSize:'500%'},1000,'easeOutBounce').delay(1000);
 	$('#order1').fadeOut(300,function(){
 		$('#order3').animate({top:'-5%',height:'95%'},1000,'easeOutExpo',function(){
-			$('#order3 h2').fadeOut();
+			$('#order3>h2').fadeOut();
 			$('#close3').fadeIn();
 		});
 
@@ -68,7 +72,7 @@ $('#close3').click(function(){
 	$('#order3').animate({height:'25%',top:'60%'},1000,'easeOutExpo',function(){
 	$('#order1').fadeIn();
 	$('#order2').fadeIn();
-	$('#order3 h2').css({fontSize:'300%'}).fadeIn();
+	$('#order3>h2').css({fontSize:'300%'}).fadeIn();
 	$('#close3').fadeOut();		
 	})
 });
