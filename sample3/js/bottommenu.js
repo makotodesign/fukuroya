6616ab92d3e8f071f3be1980fe1b08f50c9626b9
makotodesign <makoto@makotodesign.net>
2015-06-21 11:24:48 +0900

    箱出現タイミング調整

--- sample3/js/bottommenu.js
+++ sample3/js/bottommenu.js
@@ -20,20 +20,21 @@ $('#mainnav2,#mainnav3,#mainnav4').click(function(){
 var openflag = true;
 $('#bottommenu').click(function(){
 	if(openflag){	
-		$('#topwrapInner article').fadeOut('fast');
-		$('#topbtn').fadeOut('fast');
-		$('#topwrapInner').wrapInner('<div class="top_add_content"></div>');
-		$('.top_add_content').fadeIn('fast').prepend('<span id="top_add_close" class="fa fa-times-circle fa-2x" style="position:absolute;right:1%;top:1%;display:block;cursor:pointer;"></span><div class="top_add_content_detail"></div>');
-		$('#top_add_close').click(function(){
-			$('.top_add_content').fadeOut('slow');
-			$('#topwrapInner').html('').load('topcontent.html');
-			setTimeout(function(){
-			$('#bg1').animate({opacity:1},100,'linear');
-			},10);
-			$('#topbtn').fadeIn();
-			openflag = true;
-		});
-
+		$('#topwrapInner article').fadeOut(500);
+		$('#topbtn').fadeOut(500);
+		setTimeout(function(){
+			$('#topwrapInner').wrapInner('<div class="top_add_content"></div>');
+			$('.top_add_content').fadeIn('fast').prepend('<span id="top_add_close" class="fa fa-times-circle fa-2x" style="position:absolute;right:1%;top:1%;display:block;cursor:pointer;"></span><div class="top_add_content_detail"></div>');
+			$('#top_add_close').click(function(){
+				$('.top_add_content').fadeOut('slow');
+				$('#topwrapInner').html('').load('topcontent.html');
+				setTimeout(function(){
+				$('#bg1').animate({opacity:1},100,'linear');
+				},10);
+				$('#topbtn').fadeIn();
+				openflag = true;
+			});
+		},1000);
 	};
 openflag=!true;
 });
