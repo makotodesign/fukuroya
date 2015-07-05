fc56eeb87b8a2ae79b5263ab01efb855e3cf2553
makotodesign <makoto@makotodesign.net>
2015-07-05 16:40:06 +0900

    写真アップロード実験

--- master/js/order_make.js
+++ master/js/order_make.js
@@ -1,8 +1,26 @@
 $(function(){
+$('.fileInput').change(function(){
+                // 1. 選択されたファイルがない場合は何もせずにreturn
+                if (!this.files.length) {
+                	alert('ファイルが選択されていません');
+                    return;
+                }
+ 
+                var file = this.files[0],            // 2. files配列にファイルが入っています
+                    fileReader = new FileReader();   // 4. ファイルを読み込むFileReaderオブジェクト
+ 
+                // 5. 読み込みが完了した際のイベントハンドラ。imgのsrcにデータをセット
+                fileReader.onload = function(event) {
+                    // 読み込んだデータをimgに設定
+                    $('#userlogo').attr('src', event.target.result);
+                };
+ 
+                // 6. 画像読み込み
+                fileReader.readAsDataURL(file);
+            });
 
 
-
-
-
-	
 });
+
+
+
