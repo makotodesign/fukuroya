7ed81c6fbcfea8f23cd90c69450f0a0fe6db06e4
MakotoDesign <makoto@makotodesign.net>
2015-07-19 17:58:46 +0900

    module

---
+++ boot/footer.php
@@ -0,0 +1,9 @@
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.lettering-0.6.1.min.js"></script>
+    <script src="js/jquery.textillate.js"></script>
+    <script src="js/action.js"></script>
+  </body>
+</html>
---
+++ boot/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>袋屋|top</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/orig.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <div class="wrap">
+  <!--   <nav class="globalnav container-fluid">
+      <h1>fukuroya.jp</h1> 
+    </nav> -->
+    <header class="mainheader navbar container-fluid">
+
+      <div class="container text-muted">
+        <div class="navbar-header">
+          <button class="navbar-toggle" data-toggle="collapse" data-target=".target">
+          <i class="glyphicon glyphicon-menu-hamburger"></i>
+          </button>
+
+          <a class="navbar-brand">Fukuroya</a>
+        </div>
+        <nav class="collapse navbar-collapse target">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="">商品一覧</a></li>
+          <li><a href="">注文</a></li>
+          <li><a href="">会社概要</a></li>
+          <li><a href="">お問い合わせ</a></li>
+
+          </ul>
+        </nav>
+      </div>
+    </header>
