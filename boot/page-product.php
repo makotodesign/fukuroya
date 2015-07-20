7604c30248a0982a144f591931b9399f9e1f4c35
MakotoDesign <makoto@makotodesign.net>
2015-07-20 21:06:53 +0900

    product detail start

--- boot/page-product.php
+++ boot/page-product.php
@@ -1,10 +1,146 @@
 <?php require_once('header.php')?>
     <main class="product_main container">
 		<div class="page-header">
-		<h1><span id="product_title">業界最安値をあなたに </span><small id="product_subtitle">高品質、低価格には理由があります</small></h1>
+		<h1><span id="product_title">業界最速、最安値、をあなたに </span><small id="product_subtitle">高品質、低価格には理由があります</small></h1>
 		  <p id="product_p">その理由は...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 		</div>
 		<section id="product_content" class="content_box">
+			<div class="row margin-bottom-20">
+
+				<h2 class="col-md-7 col-md-offset-2 margin-top-0 text-primary"><i class="fa fa-check-square-o"></i>&nbsp;商品一覧を表示します、納期を選んで下さい</h2>
+				<div class="col-md-3">
+					<div class="btn-group ">
+					<button type="button" class="btn btn-primary"><i class="fa fa-arrow-circle-right"></i>&nbsp;納期を選択</button>
+					<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+					<span class="caret"></span>
+					<span class="sr-only">Toggle Dropdown</span>
+					</button>
+					<ul class="dropdown-menu" role="menu">
+					<li><a href="#" id="nouki_select1">5営業日</a></li>
+					<li><a href="#" id="nouki_select2">7営業日</a></li>
+					
+					</ul>
+					</div>
+				</div>
+			</div>
+			<div id="product_detail">
+				<article id="day5" class="product_select container">
+					<div class="row">
+					<h2 class="bg-primary text-center margin-top-0">納期5営業日</h2>
+					</div>
+					<div class="row product_row">
+						<div class="col-md-4 bag_inq big_bag">
+							<div class="product_box">
+								<h2><span class="rem10">L</span>サイズ</h2>
+								<p class="bagcaption">
+									雑貨店、食料品店の店舗でよく使われるサイズです。
+									雑貨、アパレル、服など様々な用途に対応出来ます。
+									背の高いトールサイズとスタンダードサイズをご用意いたしました
+									<span class="bagsize">幅32cm×奥行き11cm<br>×高さ(26.5cm or 45cm)</span>
+								</p>
+								
+
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="product_box">
+
+
+
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>
+						</div>
+					</div>
+					<div class="row product_row">
+						<div class="col-md-4 bag_inq mid_bag">
+							<div class="product_box">
+								<h2><span class="rem10">M</span>サイズ</h2>
+								<p class="bagcaption">
+									少し小振りのサイズです。小物雑貨、枚数の少ない服などでよく使われるサイズです。
+									雑貨、アパレル、服など様々な用途に対応出来ます。
+									<span class="bagsize">幅28cm×奥行き8cm<br>×高さ(26.5cm or 40cm)</span>
+								</p>							
+							</div>	
+						</div>
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>
+						</div>
+					</div>					
+					<div class="row product_row">
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>	
+						</div>
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>
+						</div>
+					</div>
+				
+						
+				</article>
+				<article id="day7" class="product_select">
+					<h2 class="bg-primary text-center margin-top-0">納期7営業日</h2>
+					<div class="row product_row">
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>	
+						</div>
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>
+						</div>
+					</div>
+					<div class="row product_row">
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>	
+						</div>
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>
+						</div>
+					</div>					
+					<div class="row product_row">
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>	
+						</div>
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="product_box">
+							</div>
+						</div>
+					</div>
+				
+						
+				</article>	
+			</div>	
+
+
 
 
 
@@ -15,5 +151,5 @@
  
     </main>
 
-  </div>
+
 <?php require_once('footer.php')?>
