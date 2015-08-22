e79422289b5da7fe6f21b50a090c8df5d078d911
MakotoDesign <makoto@makotodesign.net>
2015-08-22 15:02:54 +0900

    textarea lineheight add

--- js/kantan_order.js
+++ js/kantan_order.js
@@ -11,7 +11,7 @@ $("input"). keydown(function(e) {
 
 //袋サイズ選択	
 $('#kantan #size').on('change',function(){
-	$('html,body').animate({scrollTop:'280px'},300,'linear')
+	$('html,body').animate({scrollTop:'300px'},300,'linear')
 var flug = $(this).val();
 switch (flug){
 	case 'LS':
@@ -37,7 +37,12 @@ switch (flug){
 	$('.handle').animate({width:'200px'});
 	break;
 }
+if(flug=='LT'){
+	$('#bagwrap').css('top','-80px');
 
+}else{
+	$('#bagwrap').css('top','0px');
+}
 
 });
 // 紙色選択
@@ -74,33 +79,50 @@ $('#basecolor').on('change',function(){
 	$('.bgpoint').animate({backgroundColor:hex},1000,'linear');
 });
 
-
-// 文字入力1
-$('#input-title1').on('click blur keydown keyup keypress change',function(){
+// 文字入力
+k_title('#input-title1','#k-title1');
+function k_title(obj1,obj2){
+$(obj1).on('click blur keydown keyup keypress change',function(){
 	var text1=$(this).val();
-	$('#k-title1').text(text1);
-
+	$(obj2).text(text1);
 });
+}
 
+// textarea改行を反映して
+$('#input-title2').on('click blur keydown keyup keypress change',function(){
+	var text=$(this).val();
+	text=text.replace(/\r\n/g, "<br />");
+	text = text.replace(/(\n|\r)/g, "<br />");
+
+	$('#k-title2').html(text);
+});
 
 // 入力1 font-size
-$('#fontsize1').on('input change',function(){
+k_fontsize('#fontsize1','#size1','#k-title1');
+k_fontsize('#fontsize2','#size2','#k-title2');
+function k_fontsize(obj1,obj2,obj3){
+	$(obj1).on('input change',function(){
 	var size=$(this).val();
-	$('#size1').text(size);
-	$('.k-title').css('font-size',size+'%');
-});
-// 入力1ドラッグ移動	
-$('#k-title1').on('mousedown',function(event){
+	$(obj2).text(size);
+	$(obj3).css('font-size',size+'%');
+	});
+}
+
+// 入力1ドラッグ移動
+drag('#k-title1');
+drag('#k-title2');
+function drag(obj1){
+$(obj1).on('mousedown',function(event){
 	$(this).addClass('drag');
 	var left_limit=parseInt($('.bgpoint').css('left'));
 	var right_limit =left_limit + parseInt($('.bgpoint').width());
 	var top_limit = $('.bgpoint').offset().top;
 	var container_height=parseInt($('.bgpoint').css('height'));
 	var bottom_limit =top_limit + container_height;
-	var content_width=parseInt($('#k-title1').width());
-	var content_height=parseInt($('#k-title1').height());
-	var eX = event.pageX-parseInt($('#k-title1').css("left"));
- 	var eY = event.pageY-parseInt($('#k-title1').css("top")); 	
+	var content_width=parseInt($(obj1).width());
+	var content_height=parseInt($(obj1).height());
+	var eX = event.pageX-parseInt($(obj1).css("left"));
+ 	var eY = event.pageY-parseInt($(obj1).css("top")); 	
  	$(document).mousemove(function(event){
 	$('.drag').css("left",event.pageX - eX).css("top",event.pageY - eY);
 	if(event.pageX - eX>=right_limit-content_width){
@@ -121,6 +143,8 @@ $('#k-title1').on('mousedown',function(event){
 	$('.drag').removeClass('drag');
 	});
 });
+}
+
 // 文字色選択
 fontcolor('#fontcolor1','.k-title');
 function fontcolor(obj1,obj2){
@@ -165,6 +189,7 @@ $(obj1).on('change',function(){
 }
 // font-family
 fontfamily('#fontfamily1','#k-title1');
+fontfamily('#fontfamily2','#k-title2');
 
 function fontfamily(obj1,obj2){
 $(obj1).on('change',function(){
@@ -228,11 +253,52 @@ $(obj1).on('change',function(){
 });
 }
 // 文字間
-$('#letter_spacing1').on('change',function(){
-	var s=$(this).val();
-	$('#k-title1').css({letterSpacing:s+'rem'});
+k_letterspacing('#letter_spacing1','#k-title1');
+k_letterspacing('#letter_spacing2','#k-title2');
 
+function k_letterspacing(obj1,obj2){
+$(obj1).on('change',function(){
+	var s=$(this).val();
+	$(obj2).css({letterSpacing:s+'rem'});
+});	
+}
+
+// 行間
+
+k_lineheight('#line_height2','#k-title2');
+function k_lineheight(obj1,obj2){
+	$(obj1).on('change',function(){
+	var s=$(this).val();
+	$(obj2).css({lineHeight:s});
 });
+}
+
+// 入力エリア追加
+$('#k-input2,#k-title2').hide();
+$('#areaadd1').on('change',function(){
+	if($(this).is(':checked')){
+		$('#k-input2,#k-title2').show();
+	}else{
+		if($('#input-title2').val()==''){
+		$('#k-input2,#k-title2').hide();
+		}
+		// もしエリア2に入力があったらalert
+		if($('#input-title2').val()!==''){
+			$('#areaadd1').prop("checked",true);
+			alert('文字が入力されています');
+		}
+
+	}
+});
+
+// スクロールで袋がついてくる
+$(window).scroll(function() {
+	var svalue=$('html,body').scrollTop();
+	$('.canvas').css('top',svalue / 3);
+
+	
+});
+
 
 
 });//endjQuery
--- page-k-order.php
+++ page-k-order.php
@@ -17,12 +17,12 @@ require_once('header.php');
 				<h2 class="f-pop f-color1 text-center rem3">簡単に、あなただけの紙袋をデザインしてみましょう！</h2>
 			</div>
 		</div>
-		<div class="ctrl col-md-3 bg-primary">
+		<div class="ctrl col-md-3 bg-primary clearfix">
 			<form action="" class="form" id="kantan" method="post">
-			<h3>紙袋を選ぶ</h3>
+			<h3>サイズ&sdot;紙色&sdot;文字色</h3>
 				<div class="row">
 					
-					<div class="col-xs-7">
+					<div class="col-xs-12">
 						<div class="form-group">
 							<label class="control-label" for="size">サイズ</label>
 							<select id="size" name="size" class="form-control input-sm">
@@ -35,7 +35,11 @@ require_once('header.php');
 							</select>
 						</div>
 					</div>
-					<div class="col-xs-5">
+
+				</div>
+
+				<div class="row">
+					<div class="col-xs-6">
 						<div class="form-group">
 							<label class="control-label" for="basecolor">印刷色</label>
 							<select id="basecolor" name="basecolor" class="form-control input-sm">
@@ -52,7 +56,26 @@ require_once('header.php');
 							</select>
 						</div>
 					</div>
-				</div>
+					<div class="col-xs-6">
+							<div class="form-group">
+								<label class="fontcolor1" for="basecolor">文字色</label>
+								<select id="fontcolor1" name="fontcolor1" class="form-control input-sm">
+								　<option>選択---</option>	
+								  <option class="black" value="black">黒</option>
+								  <option class="white" value="white">白</option>
+					  			  <option class="dic003" value="Dic003">Dic003</option>
+								  <option class="dic009" value="Dic009">Dic009</option>
+								  <option class="dic016" value="Dic016">Dic016</option>
+								  <option class="dic021" value="Dic021">Dic021</option>
+								  <option class="dic032" value="Dic032">Dic032</option>
+								  <option class="dic051" value="Dic051">Dic051</option>
+								  <option class="dic080" value="Dic080">Dic080</option>
+								  <option class="dic144" value="Dic144">Dic144</option>
+								</select>
+						
+							</div>
+						</div>
+				</div>	
 				<h3>文字入力1</h3>
 
 				<div class="row">
@@ -113,41 +136,98 @@ require_once('header.php');
 
 						</div>
 				</div>
-				<h3>文字色を選ぶ</h3>
 				<div class="row">
-					<div class="col-xs-12">
+					<div class="col-xs-11 col-xs-offset-1">
+						<label class="checkbox">
+						  <input type="checkbox" id="areaadd1" value="areaadd1"> 文字入力エリアを追加
+						</label>
+					</div>
+				</div>	
+				<div id="k-input2">
+					<h3>文字入力2</h3>
+
+					<div class="row">
+						<div class="col-xs-12">
 							<div class="form-group">
-								<label class="fontcolor1" for="basecolor">文字色</label>
-								<select id="fontcolor1" name="fontcolor1" class="form-control input-sm">
-								　<option>選択---</option>	
-								  <option class="black" value="black">黒</option>
-								  <option class="white" value="white">白</option>
-					  			  <option class="dic003" value="Dic003">Dic003</option>
-								  <option class="dic009" value="Dic009">Dic009</option>
-								  <option class="dic016" value="Dic016">Dic016</option>
-								  <option class="dic021" value="Dic021">Dic021</option>
-								  <option class="dic032" value="Dic032">Dic032</option>
-								  <option class="dic051" value="Dic051">Dic051</option>
-								  <option class="dic080" value="Dic080">Dic080</option>
-								  <option class="dic144" value="Dic144">Dic144</option>
-								</select>
-						
+								<label class="control-labe2">文字をこちらに書いてください</label>
+								<textarea class="form-control input-sm" name="title2" id="input-title2" placeholder="追加入力（複数行入力可能）"></textarea>
+								<small>改行は反映されます</small>
 							</div>
 						</div>
-				</div>	
+					</div>
+		
+					<div class="row">
+						<div class="col-xs-6">
+							<div class="form-group">
+								<label class="fontfamily2" for="basecolor"><i class="fa fa-font"></i>字体</label>
+								<select id="fontfamily2" name="fontfamily2" class="form-control input-sm">
+								  <option>選択---</option>
+								  <OPTGROUP label="和文フォント">
+								   <option class="mincho" value="明朝体">明朝</option>
+								   <option class="gothic" value="ゴシック">ゴシック</option>
+								   <option class="mplus-thin" value="mplus-thin">mplus-thin</option>
+								   <option class="mplus-light" value="mplus-light">mplus-light</option>
+								   <option class="mplus-regular" value="mplus-regular">mplus-regular</option>
+								   <option class="mplus-bold" value="mplus-bold">mplus-bold</option>
+								   <option class="hiraginoMaru" value="hiraginoMaru">ヒラギノ丸ゴ Pro</option>
+								   <option class="hannari" value="はんなり明朝">はんなり明朝</option>
+								  </OPTGROUP>
+								  <OPTGROUP label="欧文フォント">
+								  <option class="ArialBlack" value="ArialBlack">ArialBlack</option>
+								  <option class="TimesNewRoman" value="TimesNewRoman">TimesNewRoman</option>
+								  <option class="CourierNew" value="CourierNew">Courier New</option>
+								  <option class="Helvetica" value="Helvetica">Helvetica</option>
+								  <option class="Marvel" value="Marvel">Marvel</option>
+								  <option class="Oswald" value="Oswald">Oswald</option>
+								  <option class="GreatVibes" value="GreatVibes">GreatVibes</option>
+								  <option class="PoiretOne" value="PoiretOne">PoiretOne</option>
+								  <option class="PermanentMarker" value="PermanentMarker">PermanentMarker</option>
+								  </OPTGROUP>
+					  		
+								</select>
+							</div>
+						</div>
+						<div class="col-xs-6">	
+							<div class="form-group">
+								<label class="control-label">大きさ<span id="size2">100</span>%</label>
+								<input type="range" id="fontsize2" name="fontsize2" class="form-control　input-sm" min="50" max="800" step="5" value="100"/>
+							</div>
+						</div>
+					</div>	
+					<div class="row">
+						<div class="col-xs-6">
+							<div class="form-group">
+								<label class="letter_spacing2">文字間</label>
+								<input type="range" id="letter_spacing2" name="letter_spacing2" class="form-control　input-sm" min="0.01" max="1.8" step="0.05" value="0.05"/>
+							</div>
+						</div>
+						<div class="col-xs-6">
+							<div class="form-group">
+								<label class="line_height2">行間</label>
+								<input type="range" id="line_height2" name="line_height2" class="form-control　input-sm" min="0.7" max="2.8" step="0.1" value="1.3"/>
+							</div>
+						</div>
+					</div>
 					
+
+
+
+				</div>
 
 
 			</form>
 
 		</div>
 		<div class="canvas col-md-9">
-			<div class="handle">
-				<?php include('svg/handle.svg'); ?>
-			</div>
-			<div class="bagbody">
-				<div class="bgpoint">
-				<p id="k-title1" class="k-title">お店の名前</p>
+			<div id="bagwrap">
+				<div class="handle">
+					<?php include('svg/handle.svg'); ?>
+				</div>
+				<div class="bagbody">
+					<div class="bgpoint">
+						<p id="k-title1" class="k-title">お店の名前</p>
+						<p id="k-title2" class="k-title">追加入力エリア</>
+					</div>
 				</div>
 			</div>
 			
